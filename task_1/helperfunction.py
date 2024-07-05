d5cf54d03bb69e95c45a98e984e36279a1574613
Dastan1835 <madanbekovdastan9@gmail.com>
2024-07-05 14:03:06 +0600

    Bir nerse

---
+++ task_1/helperfunction.py
@@ -0,0 +1,2 @@
+def summa(n_1: int, n_2: int) -> int:
+    return n_1 + n_2
---
+++ task_1/main.py
@@ -0,0 +1,6 @@
+from helperfunction import summa
+
+number_1 = int(input(""))
+number_2 = int(input(""))
+
+print(summa(number_1, number_2))
